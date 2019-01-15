517c04523482d174eafd45bc46e6f9c3c0151937
Gabor Budai <budaigabor1428@gmail.com>
2019-01-15 21:51:36 +0000

    Test of String as number

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -2,22 +2,30 @@ def return_10
   return 10
 end
 
-def add (number1, number2)
-return number1 + number2
+def add(number1, number2)
+  return number1 + number2
 end
 
-def subtract (number3, number4)
-  return number3 - number4
+def subtract(number1, number2)
+  return number1 - number2
 end
 
-def multiply (number5, number6)
-  return number5 * number6
+def multiply(number1, number2)
+  return number1 * number2
 end
 
-def divide (number7, number8)
-  return number7 / number8
+def divide(number1, number2)
+  return number1 / number2
 end
 
-def length_of_string (test_string)
-  return 21
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string1, string2)
+  return string1 + string2
+end
+
+def add_string_as_number(number1, number2)
+  return number1.to_i + number2.to_i
 end
